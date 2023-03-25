1f88745c9726e2d8409093831638559e0e2f3db6
Sergei Schneidmiller <5797221@gmail.com>
2023-03-25 15:11:32 +0300

    dz1 ready

---
+++ dz1/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Введите количество элементов массива");
+int size = Convert.ToInt32 (Console.ReadLine());
+int[] array = new int[size];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine($"Количество четных чисел в массиве: {PosElemSum(array)}");
+
+
+
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+        Console.Write($"{item} ");
+    Console.WriteLine();
+}
+
+
+int PosElemSum(int[] array)
+{
+    int count = 0;
+    foreach (int item in array)
+    {
+        if (item%2 == 0) count++;
+    }
+    return count;
+}
